f71d32e1be400898a31278594f151f17424da112
Gourav-0707 <gouravraikar540@gmail.com>
2026-07-05 23:12:03 +0530

    Delete test_parser.py

--- test_parser.py
+++
@@ -1,7 +0,0 @@
-from app.parser import extract_text_from_pdf
-
-pdf_path = "resumes/Gourav_Resume_.pdf"   # Change this to your actual PDF name
-
-text = extract_text_from_pdf(pdf_path)
-
-print(text[:2000])   # Print first 2000 characters
